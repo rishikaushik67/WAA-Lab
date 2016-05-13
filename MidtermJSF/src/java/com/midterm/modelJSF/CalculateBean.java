86ab8e98658231b3770ecb9aa23f5b7aaece4ab8
rishikaushik67 <rishi_kaushik67@yahoo.com>
2016-05-12 20:39:37 -0500

    Commiited

---
+++ MidtermJSF/src/java/com/midterm/modelJSF/CalculateBean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.midterm.modelJSF;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author rishi
+ */
+@ManagedBean
+@RequestScoped
+public class CalculateBean {
+
+    /**
+     * Creates a new instance of CalculateBean
+     */
+    public CalculateBean() {
+    }
+    
+    private String 
+}
